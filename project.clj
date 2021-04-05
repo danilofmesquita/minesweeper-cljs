c3dd48c748744334843025cefa0de2146c3c4af1
Danilo Mesquita <danilofmesquita@gmail.com>
2021-04-04 23:16:44 -0300

    Update project information

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject minesweeper "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Minesweeper implementation using ClojureScript"
+  :url "https://danilofmesquita.github.io/minesweeper-cljs"
+  :license {:name "MIT License"
+            :url "https://mit-license.org/"}
 
   :min-lein-version "2.9.1"
 
